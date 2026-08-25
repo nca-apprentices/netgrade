ead60efee8fcbc8bbe98a1480d2310647d3d299d
EliasNCA <312534981+EliasNCA@users.noreply.github.com>
2026-08-25 14:17:24 +0200

    fix: trimmed edit/delete buttons and icons on settings page

--- src/pages/home/settings/components/schoolCard/SchoolCard.tsx
+++ src/pages/home/settings/components/schoolCard/SchoolCard.tsx
@@ -83,8 +83,8 @@ const SchoolCard = ({
                     editSchoolName.trim() === school.name
                   }
                 >
-                  <IonIcon slot="icon-only" icon={checkmarkOutline} />
-                  <p className="save-text">Speichern</p>
+                  <IonIcon slot="start" icon={checkmarkOutline} />
+                  <span className="save-text">Speichern</span>
                 </IonButton>
                 <IonButton
                   className="cancel-button"
@@ -92,8 +92,8 @@ const SchoolCard = ({
                   onClick={onCancel}
                   disabled={isSavePending}
                 >
-                  <IonIcon slot="icon-only" icon={closeOutline} />
-                  <p className="cancel-text">Abbrechen</p>
+                  <IonIcon slot="start" icon={closeOutline} />
+                  <span className="cancel-text">Abbrechen</span>
                 </IonButton>
               </div>
             ) : (
@@ -103,16 +103,16 @@ const SchoolCard = ({
                   color="primary"
                   onClick={onEdit}
                 >
-                  <IonIcon slot="icon-only" icon={pencilOutline} />
-                  <p className="edit-text">Bearbeiten</p>
+                  <IonIcon slot="start" icon={pencilOutline} />
+                  <span className="edit-text">Bearbeiten</span>
                 </IonButton>
                 <IonButton
                   className="delete-button"
                   color="danger"
                   onClick={onDelete}
                 >
-                  <IonIcon slot="icon-only" icon={trashOutline} />
-                  <p className="delete-text">Löschen</p>
+                  <IonIcon slot="start" icon={trashOutline} />
+                  <span className="delete-text">Löschen</span>
                 </IonButton>
               </>
             )}
--- src/pages/home/settings/components/semesterCard/SemesterCard.tsx
+++ src/pages/home/settings/components/semesterCard/SemesterCard.tsx
@@ -88,8 +88,8 @@ const SemesterCard = ({
                     editSemesterName.trim() === semester.name
                   }
                 >
-                  <IonIcon slot="icon-only" icon={checkmarkOutline} />
-                  <p className="save-text">Speichern</p>
+                  <IonIcon slot="start" icon={checkmarkOutline} />
+                  <span className="save-text">Speichern</span>
                 </IonButton>
                 <IonButton
                   className="cancel-button"
@@ -97,8 +97,8 @@ const SemesterCard = ({
                   onClick={onCancel}
                   disabled={isSavePending}
                 >
-                  <IonIcon slot="icon-only" icon={closeOutline} />
-                  <p className="cancel-text">Abbrechen</p>
+                  <IonIcon slot="start" icon={closeOutline} />
+                  <span className="cancel-text">Abbrechen</span>
                 </IonButton>
               </div>
             ) : (
@@ -108,8 +108,8 @@ const SemesterCard = ({
                   color="primary"
                   onClick={onEdit}
                 >
-                  <IonIcon slot="icon-only" icon={pencilOutline} />
-                  <p className="edit-text">Bearbeiten</p>
+                  <IonIcon slot="start" icon={pencilOutline} />
+                  <span className="edit-text">Bearbeiten</span>
                 </IonButton>
                 <IonButton
                   className="delete-button"
@@ -117,8 +117,8 @@ const SemesterCard = ({
                   onClick={onDelete}
                   disabled={isDeleteDisabled}
                 >
-                  <IonIcon slot="icon-only" icon={trashOutline} />
-                  <p className="delete-text">Löschen</p>
+                  <IonIcon slot="start" icon={trashOutline} />
+                  <span className="delete-text">Löschen</span>
                 </IonButton>
               </>
             )}
